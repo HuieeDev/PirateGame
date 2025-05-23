3cf00ee94d6c396a094ca0297885e50aeef9fa80
Huiee <b.slaney@aol.com>
2025-05-23 17:39:50 +0100

    Proto ship uses ship stats

--- Pirate/game/entities/units/ships/proto_ship.gd
+++ Pirate/game/entities/units/ships/proto_ship.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
 @onready var forwardNode : Node2D = $Forward
-@export var f_speed = 40.0
-@export var t_speed = 3.0
+@export var ship_stats : ShipStats = preload("res://game/data/ships/fallback_ship_stats.tres")
 
 func _process(delta: float) -> void:
 	var move_direction = (forwardNode.global_position - global_position).normalized()
-	velocity = move_direction * f_speed
+	velocity = move_direction * ship_stats.forward_speed
 	move_and_slide()
 	
 	var turn_amount = Input.get_axis("TurnLeft", "TurnRight")
-	var turn_rad = deg_to_rad(turn_amount * t_speed * delta)
+	var turn_rad = deg_to_rad(turn_amount * ship_stats.turning_speed * delta)
 	rotate(turn_rad)
