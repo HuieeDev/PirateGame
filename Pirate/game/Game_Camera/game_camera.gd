895ef640756f9fa87ff56b0c6b5be85b203f553d
Huiee <b.slaney@aol.com>
2025-06-07 17:01:41 +0100

    Cleaned up the camera code

--- Pirate/game/Game_Camera/game_camera.gd
+++ Pirate/game/Game_Camera/game_camera.gd
@@ -1,19 +1,14 @@
 extends Camera2D
 
-var target_position = Vector2.ZERO
+@export var _lerp_speed := 20.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	make_current()
 
 func _process(delta):
-	acquire_target()
-	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 20))
-	
-		
-	
-func acquire_target():
-	var player_nodes = get_tree().get_nodes_in_group("player")
-	if player_nodes.size() > 0:
-		var player = player_nodes[0] as Node2D
-		target_position = player.global_position
+	_try_move_to_player(delta)
+
+
+func _try_move_to_player(delta: float):
+	if Global.player:
+		global_position = global_position.lerp(Global.player.global_position, 1.0 - exp(-delta * _lerp_speed))
