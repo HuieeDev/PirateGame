3c6c349d0a0b933d97b86966e6b4824fc07c1bb9
Huiee <b.slaney@aol.com>
2025-06-25 10:37:29 +0100

    Debug UI Items and Weapons adding
    UI for adding items and weapons setup in debug UI.
    Need to create the item adding functionality then this can be hooked up

---
+++ Pirate/game/ui/debug/debug_items.gd
@@ -0,0 +1,32 @@
+extends Panel
+
+@export var _var_name_in_item_service : String = "items"
+
+@onready var _header : Label = $Header
+@onready var _item_container :DebugItemVBoxContainer = $ScrollContainer/VBoxContainer
+@onready var _item_name_label : Label = $HBoxContainer/Label
+@onready var _add_item_button : Button = $HBoxContainer/Button
+
+var _current_item : ItemParentData = null
+
+func _ready() -> void:
+	assert(_var_name_in_item_service in ItemService, "DebugItems variable name is not in ItemService")
+	
+	var items = ItemService[_var_name_in_item_service]
+	
+	_item_container.create_buttons(items)
+	_item_container.item_selected.connect(_on_item_selected)
+	
+	_add_item_button.pressed.connect(_add_current_item)
+	
+	_header.text = _var_name_in_item_service.to_upper()
+
+
+func _on_item_selected(item_data : ItemParentData) -> void:
+	_current_item = item_data
+	_item_name_label.text = "Item: " + item_data.name
+
+
+func _add_current_item() -> void:
+	# TODO: need to get items working properly
+	pass
